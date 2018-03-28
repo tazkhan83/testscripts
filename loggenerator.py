d29169cc63ec0d48e6bcf339a6c90cc48dfeb6fa
tasvir.khan <tasvir.khan@oxygen8communications.com>
2018-03-28 09:38:49 +0100

    Adding Multi-threaded file creation

--- loggenerator.py
+++ loggenerator.py
@@ -4,29 +4,54 @@ import datetime
 import logging
 from argparse import ArgumentParser
 from logging.handlers import RotatingFileHandler
+from threading import Thread
 
 
-# Example "./loggenerator.py -l 40000 -s 60", will generate a 109MB of logfiles in one minute
+# Example "./loggenerator.py -l 10000 -s 60", will generate a 5 logfiles of 27 MB each (135MB total)
 
-parser = ArgumentParser()
+def createRotatingLog(filename):
+    print("Creating Logger For %s" % (filename))
+    logger = logging.getLogger(filename)
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(filename, maxBytes=1000000000, backupCount=1)  # 1GB MaxBytes, 2 files max, i.e. live file + 1 backup (2GB generated MAX per handler)
+    logger.addHandler(handler)
+    return logger
 
-parser.add_argument("-l", "--lps", dest="lps",
-                    help="logs lines to be written per second", metavar="Logs per second", type=int, required=True)
 
-parser.add_argument("-s", "--seconds", dest="seconds",
-                    help="number of seconds to write logs for", metavar="seconds", type=int, required=True)
+def writeToRotatingLog(logger):
+    for i in range(0, args.seconds):
+        for j in range(0, args.lps):
+            line = "[%s] Second %s LogLineNo %s" % (datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), i, j)
+            logger.info(line)
+        time.sleep(1)
 
-args = parser.parse_args()
 
-print("Writing %s loglines per second for %s seconds to file loladtest.log" % (args.lps, args.seconds))
+if __name__ == "__main__":
+    parser = ArgumentParser()
 
-logger = logging.getLogger("loadtest_logger")
-logger.setLevel(logging.INFO)
-handler = RotatingFileHandler("loadtest.log", maxBytes=1000000000, backupCount=4)  # 1GB MaxBytes, 5 files max, i.e. live file + 4 backup (5GB generated MAX)
-logger.addHandler(handler)
+    parser.add_argument("-l", "--lps", dest="lps",
+                        help="logs lines to be written per second", metavar="Logs per second", type=int, required=True)
 
-for i in range(0, args.seconds):
-    for j in range(0, args.lps):
-        line = "[%s] Second %s LogLineNo %s" % (datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), i, j)
-        logger.info(line)
-    time.sleep(1)
+    parser.add_argument("-s", "--seconds", dest="seconds",
+                        help="number of seconds to write logs for", metavar="seconds", type=int, required=True)
+
+    args = parser.parse_args()
+
+    start = time.clock()
+
+    print("Writing %s loglines per second for %s seconds to 5 files loladtestA.log->loadtestE.log" % (args.lps, args.seconds))
+
+    fileList = ["loadtestA.log", "loadtestB.log", "loadtestC.log", "loadtestD.log", "loadtestE.log"]
+
+    threadsList = []
+
+    for i in range(len(fileList)):
+        logger = createRotatingLog(fileList[i])
+        thread = Thread(target=writeToRotatingLog, args=(logger, ))
+        threadsList.append(thread)
+        thread.start()
+
+    for j in range(len(threadsList)):
+        threadsList[j].join()
+
+    print("Completed Writing Logfiles in %s seconds" % (time.clock() - start))
